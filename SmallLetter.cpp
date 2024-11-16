202af8a45f2dcb1faac4f21d7193622cb9a3b953
KRAndreev23 <KRAndreev23@codingburgas.bg>
2024-11-16 21:45:56 +0200

    Created the codes for checking the letter place, if it's uppercase and not allowing symbols

---
+++ SmallLetter.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    char arr[5];
+
+    for (int i = 0; i < 5; i++) {
+
+        cin >> arr[i];//въвеждане на думата
+
+    }
+
+    for (int i = 0; i < 5; i++) {
+
+        if (arr[i] >= 65 && arr[i] <= 90) {
+
+            arr[i] = arr[i] + 32; //ако буквата е главна, я превръща в малка
+
+        }
+        else {
+
+            arr[i] = arr[i];
+
+        }
+                
+    }
+
+    for (int i = 0; i < 5; i++) {
+
+        cout << arr[i];//това е тук за изпробване на кода, да изтрие като се съединяват кодовете
+
+    }
+
+}
+

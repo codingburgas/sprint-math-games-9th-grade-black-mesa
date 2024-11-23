d3a1f8607b35775354180cff5f238c868ffca44f
KRAndreev23 <KRAndreev23@codingburgas.bg>
2024-11-23 08:12:48 +0200

    Reuploading the original commits for the place, loweracse and symbols, since i've uploaded only the cpp files

---
+++ NoSymbolsREUPLOAD/NoSymbolsREUPLOAD/NoSymbolsREUPLOAD.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool Symbol;
+    char arr[5];
+
+    for (int i = 0; i < 5; i++) {
+
+        cin >> arr[i];
+
+    }
+
+    for (int i = 0; i < 5; i++) {
+
+        if (arr[i] < 65 || (arr[i] > 90 && arr[i] < 97) || arr[i] > 122) {
+
+            Symbol = true;
+
+        }
+
+    }
+
+    if (Symbol = true) {
+        cout << "You can't put symbols";
+    }
+
+}
